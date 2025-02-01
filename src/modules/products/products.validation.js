da09283d7ee62bcad7692fb8d2bdb047603cd499
HebaHamdan2 <hebashamdan2@gmail.com>
2025-02-01 23:45:06 +0200

    products changes

--- src/modules/products/products.validation.js
+++ src/modules/products/products.validation.js
@@ -2,15 +2,22 @@ import joi from "joi";
 import { generalFields } from "../../middleware/validation.js";
 
 export const createProduct = joi.object({
-    name: joi.string().min(3).max(25).required(),
-    description: joi.string().min(2).max(150000),
-    price: joi.number().positive().required(),
-    discount: joi.number().positive().min(1),
-    file: joi.object({
-      mainImage: joi.array().items(generalFields.file.required()).length(1),
-      subImages: joi.array().items(generalFields.file.required()).min(2).max(4),
-    }),
-    status: joi.string().valid("Active", "Inactive"),
-    categoryId: joi.string().required(),
-    subcategoryId: joi.string().required(),
-  }).required();
+  name: joi.string().min(3).max(25).required(),
+  description: joi.string().min(2).max(150000),
+  price: joi.number().positive().required(),
+  discount: joi.number().positive().min(1),
+  file: joi.object({
+    mainImage: joi.array().items(generalFields.file.required()).length(1),
+    subImages: joi.array().items(generalFields.file.required()).min(2).max(4),
+  }),
+  status: joi.string().valid("Active", "Inactive"),
+  categoryId: joi.string().required(),
+  subcategoryId: joi.string().required(),
+  variants: joi.array().items(
+    joi.object({
+      color: joi.string().required(),
+      size: joi.string().valid("S", "M", "L", "XL").required(),
+      stock: joi.number().integer().min(0).required(),
+    })
+  ).min(1).required(), 
+}).required();
